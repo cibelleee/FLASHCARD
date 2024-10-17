4b52ae37d9ce7645e9d467a0f9375e6fb313baa8
cibelleee <144125322+cibelleee@users.noreply.github.com>
2024-10-17 11:04:34 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Cibelle Curiosidades',
+    'Qual é a casa de hogwarts de Cibelle?',
+    'Corvinal'
+)
+
+criaCartao(
+    'Cibelle Curiosidades',
+    'Do que a Cibelle costuma apelidar as pessoas?',
+    'Animais'
+)
+
+criaCartao(
+    'Cibelle Curiosidades',
+    'Qual é o feriado favorito de Cibelle?',
+    'Natal'
+)
+
+criaCartao(
+    'Cibelle Curiosidades',
+    'Qual é a data de nascimento de Cibelle?',
+    '03/12/2097'
+)
+
+criaCartao(
+    'Cibelle Curiosidades',
+    'Qual é o ditado popular favorito de Cibelle?',
+    'O hábito não faz o monge'
+)
+
+criaCartao(
+    'Cibelle Curiosidades',
+    'Qual é a viagem dos sonhos de Cibelle?',
+    'Escócia'
+)
