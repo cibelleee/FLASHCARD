faa06a702c3455c3f1c157262c614d5c99c1e4bd
cibelleee <144125322+cibelleee@users.noreply.github.com>
2024-10-24 11:05:11 -0300

    Add files via upload

---
+++ Cibelle/assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Capitais',
+    'Qual é a capital da China?',
+    'Pequim'
+)
+
+criaCartao(
+    'Capitais',
+    'Qual a capital da Nova Zelândia?',
+    'Wellington'
+)
+
+criaCartao(
+    'Capitais',
+    'Qual é a capital da Escócia?',
+    'Edimburgo'
+)
+
+criaCartao(
+    'Capitais',
+    'Qual é a capital do País de Gales?',
+    'Cardiff'
+)
+
+criaCartao(
+    'Capitais',
+    'Qual a capital da Guiana Francesa?',
+    'Caiena'
+)
+
+criaCartao(
+    'Capitais',
+    'Qual a capital da Ilha de Madagascar?',
+    'Antananarivo'
+)
